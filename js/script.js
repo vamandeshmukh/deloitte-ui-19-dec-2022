ca9a93a9745cf8ee9229b44cf08c240b42139839
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-19 15:41:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,49 @@
 
-console.log('Hello world!');
+// console.log('Hello world!');
+
+
+// JS basics 
+
+// variable declaration 
+// datatypes 
+// operators 
+// functions 
+// control structure
+
+// java / c / C 
+// int num = 10;
+
+// JS 
+
+// num1 = 10; // don't use this 
+// var num2 = 20; // don't use this
+// const num3 = 30; // use this as default choice 
+// let num4 = 40; // use this for other variables  
+
+// datatypes in JS
+// - primitive datatypes
+// number, string, boolean
+// - reference datatypes
+// array, JS object, class object, function 
+
+
+let myData = 10;
+console.log(myData);
+console.log(typeof (myData));
+
+myData = 20;
+console.log(myData);
+console.log(typeof (myData));
+
+myData = 'abc';
+console.log(myData);
+console.log(typeof (myData));
+
+myData = false;
+console.log(myData);
+console.log(typeof (myData));
+
+
+
+
+
