c6124e03cebdf57570254975f67a4b147e45046a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-20 11:59:28 +0530

    js types

--- js/03-js-callbacks.js
+++ js/03-js-callbacks.js
@@ -1,12 +1,25 @@
 
 // Callbacks in JS 
 
-console.log('One');
+// console.log('One');
 
-setTimeout(() => {
-    console.log('Two');
-},
-    2000);
+// setTimeout(() => {
+//     console.log('Two');
+// },
+//     2000);
 
-console.log('Three');
+// console.log('Three');
+
+
+console.log('Start');
+const getFunValue = () => {
+    setTimeout(() => {
+        return { fun: 'Be positive!' };
+    }, 2000);
+};
+
+funValue = getFunValue();
+console.log(funValue.fun);
+
+
 
