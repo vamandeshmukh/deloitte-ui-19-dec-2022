1b50feade6881ca5570662276ff61cc0bd390bfb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-20 12:35:37 +0530

    js types

--- js/04-js-class.js
+++ js/04-js-class.js
@@ -5,12 +5,15 @@ class Employee {
     eid;
     firstName;
     salary;
-
+    constructor(eid, firstName, salary) {
+        this.eid = eid;
+        this.firstName = firstName;
+        this.salary = salary;
+    };
     work = () => {
         console.log(this.firstName + ' works...');
     };
 }
-
 // Employee obj = new Employee();
 const obj = new Employee();
 obj.work();
