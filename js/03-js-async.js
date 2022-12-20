00b84c019dfd9445e4274be786dff35753f32d04
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-20 10:54:09 +0530

    js types

--- js/03-js-async.js
+++ js/03-js-async.js
@@ -3,17 +3,24 @@
 
 // function as art to another funciton 
 
-const fun = (arg) => {
-    console.log('fun function called');
-    // code to use arg
-    // console.log(arg * 2); // use 1
-    // console.log('Hi ' + arg); // use 2
-    console.log(arg.firstName); // use 3
-};
+// const fun = (arg) => {
+//     console.log('fun function called');
+//     // code to use arg
+//     console.log(arg * 2); // use 1
+//     // console.log('Hi ' + arg); // use 2
+//     // console.log(arg.firstName); // use 3
+// };
+
+// fun(10);
+
+// let num = 11;
+// fun(num);
 
 // fun(10); // call 1
 // fun('Sonu'); // call 2
-fun({ firstName: 'asdf' }); // call 3
+// fun({ firstName: 'asdf' }); // call 3
+
+
 
 
 
@@ -33,4 +40,13 @@ fun({ firstName: 'asdf' }); // call 3
 
 // console.log('Two'); // delay
 // console.log('Three');
+
+
+const fun = (arg) => {
+    console.log('fun function called');
+    arg();
+};
+
+fun(() => { console.log('argument passed'); });
+
 
