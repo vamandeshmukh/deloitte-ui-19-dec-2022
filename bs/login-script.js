a09a14c44c9c0a6923400cba146dfc531707def8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-21 11:30:28 +0530

    bootstrap

--- bs/login-script.js
+++ bs/login-script.js
@@ -1,6 +1,16 @@
 
-// code to validate login 
 
 const loginFun = () => {
+    const userName = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log(userName, password);
+    if (userName === 'user' && password === 'pass') {
+        document.getElementById('afterLogin').innerHTML = `Hi ${userName}!`;
+        document.getElementById('loginform').reset();
+    }
+    else {
+        document.getElementById('afterLogin').innerHTML = 'Invalid credentials!';
+        document.getElementById('loginform').reset();
+    }
 
 };
