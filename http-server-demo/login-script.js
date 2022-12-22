550893430c5a518cf54b0e5ae392698bfe70276d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-12-22 11:06:26 +0530

    node-demo

---
+++ http-server-demo/login-script.js
@@ -0,0 +1,17 @@
+
+const loginFun = () => {
+    const userName = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log(userName, password);
+    if (userName === 'user' && password === 'pass') {
+        document.getElementById('afterLogin').innerHTML = `Hi ${userName}!`;
+        document.getElementById('afterInvalid').innerHTML = '';
+        document.getElementById('loginform').reset();
+    }
+    else {
+        document.getElementById('afterInvalid').innerHTML = 'Invalid credentials!';
+        document.getElementById('afterLogin').innerHTML = '';
+        document.getElementById('loginform').reset();
+    }
+};
+
